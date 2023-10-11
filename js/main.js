41398eeb99121b011e8d919eaf50f0c37ab25a89
michellechoi126 <michellechoi126@gmail.com>
2023-10-11 16:10:51 -0700

    finish main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 // Write your DOM code here!
+const redBtn = document.querySelector('.red');
+const whiteBtn = document.querySelector('.white');
+const blueBtn = document.querySelector('.blue');
+const yellowBtn = document.querySelector('.yellow');
+const bodyEl = document.querySelector('body');
+
+redBtn.addEventListener('click', changeBg);
+whiteBtn.addEventListener('click', changeBg);
+blueBtn.addEventListener('click', changeBg);
+yellowBtn.addEventListener('click', changeBg);
+
+function changeBg(e) {
+    if (e.target.classList.value === "red") {
+        bodyEl.style.backgroundColor = "red"
+    } else if (e.target.classList.value === "white") {
+        bodyEl.style.backgroundColor = "white"
+    } else if (e.target.classList.value === "blue") {
+        bodyEl.style.backgroundColor = "blue"
+    } else if (e.target.classList.value === "yellow") {
+        bodyEl.style.backgroundColor = "yellow"
+    } else {
+        console.log('please select valid button')
+    }
+}
