5580cac85f58c171553f38ada63b40f96b4d4f22
michellechoi126 <michellechoi126@gmail.com>
2023-10-11 18:13:32 -0700

    condense code to be more dry

--- js/main.js
+++ js/main.js
@@ -5,15 +5,17 @@ const bodyEl = document.querySelector('body');
 ulEl.addEventListener('click', changeBg);
 
 function changeBg(e) {
-    if (e.target.classList.value === "red") {
-        bodyEl.style.backgroundColor = "red"
-    } else if (e.target.classList.value === "white") {
-        bodyEl.style.backgroundColor = "white"
-    } else if (e.target.classList.value === "blue") {
-        bodyEl.style.backgroundColor = "blue"
-    } else if (e.target.classList.value === "yellow") {
-        bodyEl.style.backgroundColor = "yellow"
-    } else {
-        console.log('please select valid button')
-    }
+    // if (e.target.classList.value === "red") {
+    //     bodyEl.style.backgroundColor = "red"
+    // } else if (e.target.classList.value === "white") {
+    //     bodyEl.style.backgroundColor = "white"
+    // } else if (e.target.classList.value === "blue") {
+    //     bodyEl.style.backgroundColor = "blue"
+    // } else if (e.target.classList.value === "yellow") {
+    //     bodyEl.style.backgroundColor = "yellow"
+    // } else {
+    //     console.log('please select valid button')
+    // }
+    const color = e.target.dataset.color;
+    document.body.style.backgroundColor = color;
 }
